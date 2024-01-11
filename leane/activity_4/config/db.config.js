52387c73bf148fa3e4d33ec1f5ea20bfc78bda7c
GTIS <robertchristian.rosales@gmail.com>
2024-01-11 14:42:09 +0800

    activity 4

---
+++ leane/activity_4/config/db.config.js
@@ -0,0 +1,16 @@
+const mysql=require("mysql2");
+const dotenv=require("dotenv").config();
+const dbConn=mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '1234',
+    database:'grade12_db'
+})
+
+dbConn.connect(function(err){
+if (err) throw err;
+console.log ("databse connected");
+});
+
+module.exports = dbConn;
+
